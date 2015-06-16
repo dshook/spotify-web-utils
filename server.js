f833c21e5d4d95039af13e7f3fbfa79e621d6179
Dillon Shook <dshook@weblinc.com>
2015-06-16 17:38:01 -0400

    Async ftw, and actually not duping now

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ var redirectUrl = 'http://localhost:' + port + '/auth';
 var playlistName = 'Old School';
 var songs = [];
 
+function songSelect(s){ return {
+    name : s.name,
+    id : s.id,
+    artist: s.artists[0].name
+  }; 
+}
+
 app.get('/scrape', function(req, res){
 
   request(url, function(error, response, html){
@@ -24,13 +31,13 @@ app.get('/scrape', function(req, res){
       $('ul#ulPlaylist>li').each(function(){
         var data = $(this);
         songs.push({
-          title: data.find('a.playlist-music').first().text(),
-          artist: data.find('.music-desc label').first().text()
+          title: data.find('a.playlist-music').first().text().trim().replace('...', ''),
+          artist: data.find('.music-desc label').first().text().trim().replace('...', '')
         });
       });
     }
 
-    console.log("Fetched " + songs.length + " songs");
+    console.log('Fetched ' + songs.length + ' songs');
 
     // requests authorization
     var scope = 'user-read-private playlist-read-private playlist-modify-private playlist-modify-public';
@@ -45,12 +52,10 @@ app.get('/scrape', function(req, res){
   });
 });
 
-app.get('/auth', function(req, res){
+app.get('/auth', async function(req, res){
 
   var code = req.query.code || null;
   var state = req.query.state || null;
-  var user = null;
-  var playlist = null;
 
   console.log('auth');
 
@@ -60,89 +65,103 @@ app.get('/auth', function(req, res){
     redirectUri: redirectUrl
   });
 
-  spotifyApi.authorizationCodeGrant(code)
-  .then(function(data) {
-    console.log('The token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
-    console.log('The refresh token is ' + data.body['refresh_token']);
+  try{
+    var authData = await spotifyApi.authorizationCodeGrant(code);
+
+    console.log('The token expires in ' + authData.body['expires_in']);
+    console.log('The access token is ' + authData.body['access_token']);
+    console.log('The refresh token is ' + authData.body['refresh_token']);
 
     // Set the access token on the API object to use it in later calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
+    spotifyApi.setAccessToken(authData.body['access_token']);
+    spotifyApi.setRefreshToken(authData.body['refresh_token']);
 
-    return spotifyApi.getMe();
-  })
-  .then(function(userData){
-    //console.log('Some information about the authenticated user', userData.body);
-    user = userData.body;
-    return user;
-  })
-  .then(function(userData){
-    return spotifyApi.getUserPlaylists(user.id)
-      .then(function(playlistData){
-        console.log('fetched playlists');
+    var userData = await spotifyApi.getMe();
+    var user = userData.body;
 
-        if(playlistData.body.items.length > 0){
-          for (var i = playlistData.body.items.length - 1; i >= 0; i--) {
-            var p = playlistData.body.items[i];
-            console.log('playlist ', p.name);
-            if(p.name === playlistName){
-              return p;
-            }
-          }
+    var playlistData = await spotifyApi.getUserPlaylists(user.id);
+    console.log('fetched playlists');
+    var playlist = null;
+
+    if(playlistData.body.items.length > 0){
+      for (var i = playlistData.body.items.length - 1; i >= 0; i--) {
+        var p = playlistData.body.items[i];
+        if(p.name === playlistName){
+          playlist = p;
         }
+      }
+    }else{
+      //create playlist if we couldn't find it
+      var playlistResult = await spotifyApi.createPlaylist(user.id, playlistName, { 'public' : false });
+      playlist = playlistResult.body;
+    }
 
-        //fall through create playlist if we couldn't find it
-        return spotifyApi.createPlaylist(user.id, playlistName, { 'public' : false })
-          .then(function(createdPlaylist){
-            return createdPlaylist.body;
-          });
-      });
-  })
-  .then(function(playlistData) {
-    playlist = playlistData;
     console.log('Got playlist');
 
     //lookup songs
-    var songPromises = [];
-    songs.forEach(function(song){
-      songPromises.push(
-        spotifyApi.searchTracks(song.title + '+artist:' + song.artist)
-          .then(function(songData){
-            //console.log(songData.body.tracks.items);
-            if(songData.body.tracks.items.length > 0){
-              return songData.body.tracks.items[0];
-            }
-            return null;
-          })
-          .catch(function(songError){
-            console.log('Error finding song');
-            console.log(songError);
-          })
-      );
-    });
+    var spotifySongs = [];
+    for(let song of songs){
+      try{
+        var searchResult = await spotifyApi.searchTracks(song.title + ' ' + song.artist);
+        console.log('search for ' + song.title + ' ' + song.artist, 
+          searchResult.body.tracks.items.slice(0, 5).map(songSelect));
+        if(searchResult.body.tracks.items.length > 0){
+          spotifySongs.push(searchResult.body.tracks.items[0]);
+        }
+      }catch (songError){
+        console.log('Error finding song');
+        console.log(songError);
+      }
+    }
 
-    return Promise.all(songPromises);
-  })
-  .then(function(songArray){
-    var trackLookups = songArray
-    .filter(function(m){ return m && m.id; })
-    .map(function(m){
-      return 'spotify:track:' + m.id;
+    //find songs already in playlist to dedupe
+    var playlistSongIds = [];
+    var offset = 0;
+    var limit = 100;
+    var tracksFetched = 0;
+    do{
+      var playlistTrackData = await spotifyApi.getPlaylistTracks(user.id, playlist.id, 
+        { 'offset' : offset, 'limit' : limit, 'fields' : 'items(track(id))' });
+      console.log('playlist tracks');
+
+      tracksFetched = playlistTrackData.body.items.length;
+      offset += tracksFetched;
+      playlistSongIds = playlistSongIds.concat(
+        playlistTrackData.body.items.map(s => s.track.id)
+      );
+
+    }while(tracksFetched === limit);
+
+    console.log('Fetched existing tracks');
+
+    var actualNewSongs = [];
+    //find new tracks
+    for(let potentialNewSong of spotifySongs){
+      if(playlistSongIds.indexOf(potentialNewSong.id) === -1){
+        actualNewSongs.push(potentialNewSong);
+      }
+    }
+
+    var trackLookups = actualNewSongs
+      .filter(function(m){ return m && m.id; })
+      .map(function(m){
+        return 'spotify:track:' + m.id;
+      });
+
+    var playlistAdd = spotifyApi.addTracksToPlaylist(user.id, playlist.id, trackLookups);
+
+    console.log('playlist songs added');
+    res.json({
+      songsAdded: actualNewSongs.map(songSelect),
+      scrapedSongs: songs,
+      potentialNewSongs: spotifySongs.map(songSelect)
     });
-    console.log(trackLookups);
-    return spotifyApi.addTracksToPlaylist(user.id, playlist.id, trackLookups);
-  })
-  .then(function(playlistAdd){
-    console.log('playlist songs added', playlistAdd);
-    res.json({code: code, playlistAdd: playlistAdd});
     res.end();
-  })
-  .catch(function(err){
+  } catch (err){ 
     console.log('Something went wrong!', err);
     res.write('Something went wrong!' + JSON.stringify(err));
     res.end();
-  });
+  }
 
 });
 
