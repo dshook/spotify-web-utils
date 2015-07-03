c83630c0e945b154c0e8261b4def88bfce453189
Dillon Shook <dshook@alumni.nmt.edu>
2015-07-02 20:45:40 -0400

    App working mounted under arbitrary path
    As long as it's added in the spotify dev tools

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ var port = process.env.PORT || '8081';
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
 
+var router = express.Router();
 var spotifyApp = new SpotifyApp(port, clientId, clientSecret);
 
-app.get('/auth', spotifyApp.authenticate.bind(spotifyApp));
-app.get('/scrape', spotifyApp.scrape.bind(spotifyApp));
-app.get('/coverify', spotifyApp.coverify.bind(spotifyApp));
-app.get('/playlists', spotifyApp.playlists.bind(spotifyApp));
+router.get('/auth', spotifyApp.authenticate.bind(spotifyApp));
+router.get('/scrape', spotifyApp.scrape.bind(spotifyApp));
+router.get('/coverify', spotifyApp.coverify.bind(spotifyApp));
+router.get('/playlists', spotifyApp.playlists.bind(spotifyApp));
+
+
+app.use('/spotify', router);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
